56dd77fd47912ecebe6093da882e508ae3eb1729
Alice <agenkin1@gmail.com>
2020-06-20 19:14:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,13 @@
 class Dog
   def initialize(breed)
-     @breed = breed
-   end
+   @breed = breed
+ end
 
-   def breed=(breed)
-     @breed = breed
-   end
+ def breed=(breed)
+   @breed = breed
+ end
 
-   def breed
-     @breed
-   end
-
-  def initialize(name)
-     @name = name
-   end
-
-   def name=(name)
-     @name = name
-   end
-
-   def name
-     @name
-   end
+ def breed
+   @breed
+ end
 end
