918b89960ad15add28c24c6e368ed4f28655d24f
Alice <agenkin1@gmail.com>
2020-06-20 19:05:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class dog
+  def initialize(breed)
+     @breed = breed
+   end
+
+   def breed=(breed)
+     @breed = breed
+   end
+
+   def breed
+     @breed
+   end
+ end
+
+  def initialize(name)
+     @name = name
+   end
+
+   def name=(name)
+     @name = name
+   end
+
+   def name
+     @name
+   end
+ end
+end
